5524f5ad1c6e3c6aa9968843593ee153c63d66bd
min3076 <vial222@gmail.com>
2023-12-05 11:39:18 +0900

    first1207

--- js/include.js
+++ js/include.js
@@ -1,11 +1,3 @@
 $(function(){
-    $("#header").load("./include/header.html"); 
-    $("#footer").load("./include/footer.html"); 
-    
-    $("#pop-test1").load("./include/pop_test1.html"); // modal 진단하기 - 읽기 
-    $("#pop-test2").load("./include/pop_test2.html"); // modal 진단하기 - 유창성 
-    $("#pop-test3").load("./include/pop_test3.html"); // modal 진단하기 - 쓰기
-
-    $("#pop-study1").load("./include/pop_study1.html"); // modal 학습하기
-
-    });
+    $("#question_header").load("../include/header.html"); 
+});
